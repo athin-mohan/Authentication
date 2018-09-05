96b7f6e3974ecfeb82decf714f2cd5c1ad306f8f
athin-mohan <athin-mohan@github.com>
2018-09-06 02:51:35 +0530

    Changes in controller
    Now only it prints username and password from Header

--- BasicToFormBased/src/com/capgemini/controller/AuthenticationController.java
+++ BasicToFormBased/src/com/capgemini/controller/AuthenticationController.java
@@ -4,7 +4,9 @@ import java.io.Console;
 
 
 
+
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,51 +14,31 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AuthenticationController {
-
-	@RequestMapping("/showHomePage")
-	public String showHomepage()
-	{
-		return "Homepage";
-		
-	} 
-	@SuppressWarnings("null")
-	@RequestMapping(value = "/sendrequest", method = RequestMethod.POST)
-	public HttpServletResponse sendRequest(@RequestParam("uname") String username,@RequestParam("pwd") String pwd )
-	{
-		HttpServletResponse response1 = null;
-		//HttpServletRequest request;
-		String uandp= username+":"+pwd;
-		String encodedup =Base64Utils.encodeToString(uandp.getBytes());
-		response1.setHeader("Authorization","Basic "+encodedup );
-		return response1;
-		
-	}
 	
-	
-	
-	@SuppressWarnings("null")
 	@RequestMapping(value = "/getFromSS", method = RequestMethod.POST)
-	public HttpServletResponse getBasicAndSendForm(@RequestHeader String header,
-			@RequestBody String body) {
-		HttpServletResponse response = null;
-		if (header != null && header.startsWith("Basic ")) {
-		String encodedcred=header.substring(6).trim();
-		String decodedcred=Base64Utils.decodeFromString(encodedcred).toString();
+	public String getBasicAndSendForm(@RequestHeader(value="Authorization") String header) 
+	{
+		System.out.println("header is "+header);
+		int len=("Authorization:Basic ").length();
+		if (header != null && header.startsWith("Authorization: Basic ")) {
+			
+	    System.out.println("Header is for basic Authentication");
+		String encodedcred=header.substring(len).trim();
+		String decodedcred=new String(Base64Utils.decodeFromString(encodedcred));
 		String decodedUname=decodedcred.substring(0,decodedcred.indexOf(":"));
 		String decodedPwd=decodedcred.substring(decodedcred.indexOf(":")+1);
-		System.out.println(decodedUname);
-		System.out.println(decodedPwd);
-		Console console = null;
-		console.printf(decodedUname, decodedUname);
-		console.printf(decodedPwd, decodedPwd);
-		response.setHeader("Uname",decodedUname);
-		response.setHeader("Uname",decodedPwd);
-		return response;
+		System.out.println("decoded Username is: "+decodedUname);
+		System.out.println("decoded Password is: "+decodedPwd);
+	
+		return "Success: "+decodedUname+decodedPwd;
 		} else {
-			return response;
+			
+			System.out.println("Header for basic authentication not found");
+			return "Failed";
 		}
 
 	}
